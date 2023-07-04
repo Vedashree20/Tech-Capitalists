80c5882f2c37e08db8d268f9993a8790bdf4a818
Mihika Dakappagari <95414941+Mihika135@users.noreply.github.com>
2023-07-04 23:45:27 +0530

    Delete Frontend UI/src directory

--- Frontend UI/src/Pages/Notifications.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Notifications() {
-  return (
-    <div className='container2'><div className='heading'>Set Alerts/Notifications</div></div>
-  )
-}
-
-export default Notifications
